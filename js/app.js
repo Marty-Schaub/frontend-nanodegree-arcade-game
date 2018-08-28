397de90cac42b73fc415851a0236f5e0b5477d8c
Marty Schaub <marty.schaub@gmail.com>
2018-08-28 07:26:27 -0400

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -21,16 +21,31 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+const Character = function(){
+  this.sprite='images/char-boy.png';
+  this.startX = 200;
+  this.startY = 200;
+};
+
+Character.prototype.update = function(dt1) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.person), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+
 // Place the player object in a variable called player
-
-
+let player = new Character();
+let enemy1 = new Enemy();
+let enemy2=new Enemy();
+let allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
